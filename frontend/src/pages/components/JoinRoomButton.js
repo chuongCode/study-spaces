136ee834e4bec9374138106f31560d7db0fa9098
chuongCode <rchuong@u.rochester.edu>
2024-02-24 14:26:41 -0500

    Websockets Initial

---
+++ frontend/src/pages/components/JoinRoomButton.js
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';  
+import { useState } from 'react';
+import { io } from "socket.io-client";
+
+let socket;
+
+const JoinRoomButton = ({ room }) => {
+    const [socket, setSocket] = useState(null);
+  useEffect(() => {
+    const socket = io("http://localhost:3005"); // replace with your server URL
+    setSocket(socket);
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const joinRoom = () => {
+    if (socket) {
+        console.log('click')
+        socket.emit('join-room', room);
+    }
+    
+  };
+
+  return (
+    <button onClick={joinRoom}>
+      Join Room
+    </button>
+  );
+};
+
+export default JoinRoomButton;
