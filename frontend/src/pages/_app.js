d7114a9370152d5f4ca98a20176d9960fbdfaf65
Kayla Lin <klin23@u.rochester.edu>
2024-02-25 11:03:27 -0500

    red

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -2,12 +2,15 @@ import '@/styles/globals.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import Starfield from '@/components/StarField';
 import { SocketProvider } from '@/zustand/socket';
+import AnimatedCursor from 'react-animated-cursor';
+
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
     return (
         <SocketProvider>
             <QueryClientProvider client={queryClient}>
+                <AnimatedCursor />
                 <Component {...pageProps} />
                 <Starfield />
             </QueryClientProvider>
