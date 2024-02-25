329ae7753ea39a2ce1ab55a7e9e91c27e2095225
Jassem <toumijassem@gmail.com>
2024-02-25 04:36:26 -0500

    fix:

--- frontend/src/zustand/store.js
+++ frontend/src/zustand/store.js
@@ -2,9 +2,12 @@ import { create } from 'zustand';
 
 const useStore = create(set => ({
     username: null,
+    spaceName: null,
     //function to set the username
     setUsername: username => set({ username }),
     removeUsername: () => set({ username: null }),
+    setSpaceName: spaceName => set({ spaceName }),
+    removeSpaceName: () => set({ spaceName: null }),
 }));
 
 export default useStore;
