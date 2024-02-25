2e1fd98c827c0eb015b772fe5862100d8f92cf40
Kayla Lin <klin23@u.rochester.edu>
2024-02-25 06:32:27 -0500

    feat:

--- frontend/src/components/PlayerCard.js
+++ frontend/src/components/PlayerCard.js
@@ -3,9 +3,9 @@ import { SpaceShip1 } from './SpaceShip1';
 
 const PlayerCard = ({ playerName }) => {
     return (
-        <div className='flex flex-col items-center justify-center p-5 h-[400px]   border-2 border-solid rounded'>
+        <div className='w-[300px] flex flex-col items-center justify-center p-5 h-[400px]   border-2 border-solid rounded'>
             <div style={{ opacity: playerName ? '100%' : '30%' }}>
-                <SpaceShip1 size={300} className='rotate-[50deg] relative left-[50px]' />
+                <SpaceShip1 size={100} className='rotate-[45deg] relative left-[0px] top-[-20px]' />
             </div>
             {playerName ? (
                 <h2 className='text-2xl font-extralight uppercase'>{playerName}</h2>
